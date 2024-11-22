dd4b77efbf1656ab0b5e275d7416e9a316aa2c30
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2024-11-22 21:44:30 +0530

    Update ResponseInternalRatings.js

--- ResponseInternalRatings.js
+++ ResponseInternalRatings.js
@@ -1,10 +1,39 @@
+// package com.socgen.dbe.Model;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class ResponseInternalRatings {
+//     private List<Sensitivity> sensitivityList = new ArrayList<>();
+
+//     public List<Sensitivity> getSensitivityList() {
+//         return sensitivityList;
+//     }
+
+//     public void setSensitivityList(List<Sensitivity> sensitivityList) {
+//         this.sensitivityList = sensitivityList;
+//     }
+
+//     @Override
+//     public String toString() {
+//         return "ResponseInternalRatings{" +
+//                "sensitivityList=" + sensitivityList +
+//                '}';
+//     }
+// }
+
+
+
 package com.socgen.dbe.Model;
 
-import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ResponseInternalRatings {
-    private List<Sensitivity> sensitivityList = new ArrayList<>();
+    @JsonProperty("sensitivityList")
+    private List<Sensitivity> sensitivityList;
 
     public List<Sensitivity> getSensitivityList() {
         return sensitivityList;
