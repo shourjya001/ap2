c1711922c9ca6400331153549ecb20e9a0f84cfa
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2024-11-22 21:51:39 +0530

    Update ResponseInternalRatings.js

--- ResponseInternalRatings.js
+++ ResponseInternalRatings.js
@@ -24,16 +24,46 @@
 
 
 
+// package com.socgen.dbe.Model;
+
+// import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+// import com.fasterxml.jackson.annotation.JsonProperty;
+// import java.util.List;
+
+// @JsonIgnoreProperties(ignoreUnknown = true)
+// public class ResponseInternalRatings {
+//     @JsonProperty("sensitivityList")
+//     private List<Sensitivity> sensitivityList;
+
+//     public List<Sensitivity> getSensitivityList() {
+//         return sensitivityList;
+//     }
+
+//     public void setSensitivityList(List<Sensitivity> sensitivityList) {
+//         this.sensitivityList = sensitivityList;
+//     }
+
+//     @Override
+//     public String toString() {
+//         return "ResponseInternalRatings{" +
+//                "sensitivityList=" + sensitivityList +
+//                '}';
+//     }
+// }
+
+
+
 package com.socgen.dbe.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ResponseInternalRatings {
     @JsonProperty("sensitivityList")
-    private List<Sensitivity> sensitivityList;
+    private List<Sensitivity> sensitivityList = new ArrayList<>();
 
     public List<Sensitivity> getSensitivityList() {
         return sensitivityList;
